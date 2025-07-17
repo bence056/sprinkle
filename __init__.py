9e802d21ee11e7bc7c16ea2a5ae67dbd1f922a78
bence056 <vargabencecsaba2005@gmail.com>
2025-07-17 14:48:16 +0000

    Installed Lit and frontend preps

--- __init__.py
+++ __init__.py
@@ -1,15 +1,22 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 import logging
-from .const import *
+
+from .const import DOMAIN
+from .panel import (
+    async_register_panel,
+    async_unregister_panel
+)
 
 from homeassistant.helpers.device_registry import async_get as async_get_device_registry
-from homeassistant.components import panel_custom
+
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
+
     # Create the controller device
     device_registry = async_get_device_registry(hass)
     device = device_registry.async_get_or_create(
@@ -25,30 +32,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {"entities": []})
     hass.data[DOMAIN]["device_id"] = device.id
 
+    #Setup the side panel.
+    await panel.async_register_panel(hass)
+
     # create the default controller entities after controller flow creation.
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-
-    async def handle_run_zone(call):
-        entity_id = call.data["entity_id"]
-        duration = call.data["duration"]
-        for entity in hass.data[DOMAIN]["entities"]:
-            if entity.entity_id == entity_id:
-                await entity.async_run_zone(duration)
-                break
-
-    hass.services.async_register(DOMAIN, "run_zone", handle_run_zone)
-
-    # Reguister the custom panel
-    await panel_custom.async_register_panel(
-        hass,
-        webcomponent_name=DOMAIN,
-        frontend_url_path="",
-        module_url="",
-        sidebar_title="Irrigation Config",
-        sidebar_icon="mdi:sprinkler-variant",
-        require_admin=True,
-        config={},
-        config_panel_domain=DOMAIN,
-    )
 
     return True
--- const.py
+++ const.py
@@ -2,3 +2,13 @@ DOMAIN = "sprinkle"
 NAME = "Sprinkle Virtual Irrigation"
 VERSION = "1.0.0"
 MANUFACTURER = "bence056"
+
+CUSTOM_COMPONENTS = "custom_components"
+INTEGRATION_FOLDER = DOMAIN
+PANEL_FOLDER = "frontend"
+PANEL_FILENAME = "src/sprinkle-panel.js"
+
+PANEL_URL = "/api/panel_custom/sprinkle"
+PANEL_TITLE = NAME
+PANEL_ICON = "mdi:sprinkler-variant"
+PANEL_NAME = "alarm-panel"
