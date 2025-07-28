88e68d0538d594cf7008735044d90e4293f2fbf0
bence056 <vargabencecsaba2005@gmail.com>
2025-07-28 13:01:22 +0200

    Further work on cpanel

--- frontend/src/sprinkle-panel.ts
+++ frontend/src/sprinkle-panel.ts
@@ -25,7 +25,9 @@ export class SprinklePanel extends LitElement {
             </ha-card>
             <ha-card header="Cycles">
                 <div class="card-content">
-                    
+                    <ha-selector-icon label="Test Label"></ha-selector-icon>
+                    <ha-control-select-menu options=${['a', 'b', 'c']} label="Select from here" show-arrow=true></ha-control-select-menu>
+                    <ha-textfield></ha-textfield>
                 </div>
             </ha-card>
             <ha-card header="Schedules">
