aed0aeeb6f2321d3e20bea6be7ca1a92b966df07
bence056 <vargabencecsaba2005@gmail.com>
2026-05-22 20:01:44 +0200

    Added valve toggle delay

--- frontend/src/panels/general-panel.ts
+++ frontend/src/panels/general-panel.ts
@@ -12,6 +12,8 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
 
     hass!: HomeAssistant;
     settingsObject!: GeneralSettings;
+    @state()
+    modifiedValveDelay!: number;
 
 
     protected hassSubscribe(): Array<UnsubscribeFunc | Promise<UnsubscribeFunc>> {
@@ -23,6 +25,7 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
         if(!this.hass) return;
 
            this.settingsObject = await getGeneralSettings(this.hass);
+           this.modifiedValveDelay = this.settingsObject.valve_toggle_delay_ms;
            this.requestUpdate();
 
     }
@@ -39,7 +42,7 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
         commonStyle,
         css`
         
-            .horizontal-split {
+            .master-valve-config {
                 display: grid;
                 grid-template-areas: 'label switch' 'dropdown dropdown';
                 grid-template-rows: auto auto;
@@ -47,6 +50,18 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
                 justify-content: start;
                 align-content: center;
                 width: 30%;
+                margin-bottom: 30px;
+            }
+            
+            .valve-delay-config {
+                display: grid;
+                grid-template-areas: 'label hint' 'input input' 'save -';
+                grid-template-rows: 1fr 1fr auto;
+                grid-template-columns: auto auto;
+                justify-content: start;
+                align-content: center;
+                width: 30%;
+                margin-bottom: 30px;
             }
             
         `
@@ -56,13 +71,13 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
         return html`
             <div class="section">
                 <ha-card header="General Settings">
-                <ha-expansion-panel expanded=true header="Master Valve">
-                    <div class="horizontal-split">
+                <ha-expansion-panel expanded=true header="Valves">
+                    <div class="master-valve-config">
                         <ha-switch .checked=${this.settingsObject?.use_master_valve || false} @change="${(e: Event) => {
                             this.settingsObject.use_master_valve = (e.target as any).checked;
                             this.onSettingsChanged();
                         }}" style="grid-area: switch; justify-content: space-around"></ha-switch>
-                        <p style="grid-area: label; font-size: 18px; margin-right: 20px">Use master valve</p>
+                        <p style="grid-area: label; font-size: 18px; margin-right: 20px; display: flex; align-items: center">Use master valve</p>
                         <ha-select .value=${this.settingsObject?.master_valve_entity_id || ""}
                                    .disabled = ${this.settingsObject?.use_master_valve == false}
                                    .options=${getValveEntities(this.hass)
@@ -79,7 +94,17 @@ export class ZonePanel extends SubscribeMixin(LitElement) {
                                     @closed=${(e: CustomEvent) => e.stopPropagation()}
                                    style="grid-area: dropdown"></ha-select>
                     </div>
-                </ha-expansion-panel>
+                    <div class="valve-delay-config">
+                        <p style="font-size: 18px; margin-right: 5px; grid-area: label; display: flex; align-items: center">Valve transition delay (ms)</p>
+                        <ha-icon id="help" style="grid-area: hint; display: flex; align-items: center" icon="mdi:help-circle"></ha-icon>
+                        <ha-tooltip for="help">The minimum delay that has to pass between stopping a valve and starting the next one</ha-tooltip>
+                        <ha-input .value=${this.modifiedValveDelay}
+                                  @input=${(e: Event) => this.modifiedValveDelay = Number((e.target as HTMLInputElement).value)}
+                                  label="0 ms" type="number" min="0" max="5000" step="100" style="grid-area: input"></ha-input>
+                        ${ this.settingsObject.valve_toggle_delay_ms != this.modifiedValveDelay ? 
+                                html`<ha-button style="grid-area: save" @click=${() => {this.settingsObject.valve_toggle_delay_ms = this.modifiedValveDelay; this.onSettingsChanged();} }>Save</ha-button>` : ""}
+                     
+                </ha-expansion-panel>    
                 </ha-card>
             </div>
             
