2e13021c75cac968dcc4f93569f1670e660cb3ce
bence056 <vargabencecsaba2005@gmail.com>
2026-05-24 20:01:58 +0200

    added back button to sprinkle panel

--- custom_components/sprinkle/frontend/src/sprinkle-panel.ts
+++ custom_components/sprinkle/frontend/src/sprinkle-panel.ts
@@ -9,11 +9,34 @@ import "./panels/general-panel"
 @customElement('sprinkle-panel')
 export class SprinklePanel extends LitElement {
   @property({ attribute: false }) hass!: HomeAssistant;
+  @property({ type: Boolean, reflect: true }) public narrow!: boolean;
 
-  static styles = commonStyle;
+  static styles = [
+      commonStyle,
+      css`
+          .header {
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            height: 50px;
+            background: var(--primary-color);
+            display: flex;
+            align-items: center;
+            justify-content: start;
+          }
+          
+      `
+  ];
 
   render() {
+      console.log(this.narrow);
     return html`
+        
+        <div class="header">
+            ${this.narrow ? html`
+            <ha-menu-button .hass=${this.hass} .narrow=${this.narrow}></ha-menu-button>` : null}
+        </div>
+        
         <general-panel .hass="${this.hass}"></general-panel>
         <zone-panel .hass="${this.hass}"></zone-panel>
         <cycle-panel .hass="${this.hass}"></cycle-panel>
