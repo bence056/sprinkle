dd77952b97a6f44d520806842857e9453557a509
Varga Bence Csaba <vargabencecsaba2005@gmail.com>
2026-05-31 01:12:15 +0200

    added async lock to setup_manager

--- custom_components/sprinkle/setup_manager.py
+++ custom_components/sprinkle/setup_manager.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from typing import cast
 
@@ -21,6 +22,8 @@ class SprinkleSetupManager:
     def __init__(self, hass: HomeAssistant, store: SprinkleStorage):
         self.hass = hass
         self.store_obj: SprinkleStorage = store
+        self._lock = asyncio.Lock()
+        self._count = 0
 
     def get_registered_config_entries(self):
         return_array = []
@@ -42,15 +45,21 @@ class SprinkleSetupManager:
         await self.hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
         await coordinator.async_setup()
-        is_first_entry = len(self.hass.data[DOMAIN]["config_entries"]) == 1
 
-        if is_first_entry:
-            # Setup the side panel.
-            _LOGGER.info(f"Setting up Sprinkle Panel.")
-            await async_register_panel(self.hass)
-            # Register the websockets.
-            _LOGGER.info(f"Registering websockets.")
-            register_websockets(self.hass)
+        #
+        # FOR SOME REASON, PANEL TRIES TO GET REGISTERED FOR EVERY CONFIG.
+        #
+
+
+        async with self._lock:
+            if self._count == 0:
+                self._count += 1
+                # Register the websockets.
+                _LOGGER.info(f"Registering websockets.")
+                register_websockets(self.hass)
+                # Setup the side panel.
+                _LOGGER.info(f"Setting up Sprinkle Panel.")
+                await async_register_panel(self.hass)
 
 
     async def async_unload(self, entry: ConfigEntry):
@@ -58,9 +67,12 @@ class SprinkleSetupManager:
         coordinator: SprinkleCoordinator = try_get_coordinator(self.hass, entry)
         await coordinator.async_unload()
         del self.hass.data[DOMAIN]["config_entries"][entry.entry_id]
-        is_last_entry = len(self.hass.data[DOMAIN]["config_entries"]) == 0
-        if is_last_entry:
-            async_unregister_panel(self.hass)
+
+        async with self._lock:
+            self._count -= 1
+            if self._count == 0:
+                async_unregister_panel(self.hass)
+
         await self.hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
